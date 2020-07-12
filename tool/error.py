5b0d78990e3ee2c2d9a2be538662e0f384f7ad67
sky20041129 <46390548+sky20041129@users.noreply.github.com>
2020-07-12 13:16:25 +0800

    Create error.py

---
+++ tool/error.py
@@ -0,0 +1,28 @@
+#coding:utf-8
+'''
+Created on 2010/12/21
+
+@author: panda.huang
+'''
+
+class onqaserror(Exception): pass
+
+class noqafile(Exception): pass
+
+class notfindqafile(Exception): pass
+
+class notfindinifile(Exception): pass
+
+class notfindincludefile(Exception): pass
+
+class nonamevalue(Exception): pass
+
+class equalerror(Exception): pass
+
+class notequalerror(Exception): pass
+
+class notfind(Exception): pass
+
+class find(Exception): pass
+
+class notfindappfile(Exception): pass
